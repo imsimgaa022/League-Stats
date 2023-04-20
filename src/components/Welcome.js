c06a15920aebaa52d05e447bab3b3b7d72580ae8
Miki <miroslav_simic@epam.com>
2023-04-20 16:24:33 +0200

    Additonal message on welcome modal

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -48,8 +48,8 @@ const Welcome = () => {
         }}
       >
         <Modal maskClosable={false} onCancel={handleModalCancel} footer={null} open={showModal}>
-          <h1 className="welcome-modal-heading" style={{textAlign:"center"}}>Hello and Welcome to my project!</h1>
-          <p style={{textAlign:"center"}}>Please note that this site is still a work in progress, but I wanted to share my progress with you. I'm aware there are a lot of bugs but I'm actively working on it and will be adding new features and content regularly. Thank you for your patience and understanding, and I hope you enjoy exploring what's currently available.</p>
+          <h1 className="welcome-modal-heading text-center">Hello and Welcome to my project!</h1>
+          <p className="text-center">Please note that this site is still a work in progress, but I wanted to share my progress with you. I'm aware there are a lot of bugs but I'm actively working on it and will be adding new features and content regularly. Thank you for your patience and understanding, and I hope you enjoy exploring what's currently available.</p>
           <p>Quick tips:
             <ul>
               <li>App is made for searching League of Legends players and displaying their stats that are not avaiable in the game!</li>
@@ -57,7 +57,8 @@ const Welcome = () => {
               <li>Right now there is only option for searching players on Europe Nordic and East Server!</li>
             </ul>
           </p>
-          <h2 style={{textAlign:"center"}}>Miroslav</h2>
+          <p className="text-center">App is currently optimized for desktop use only and will not function optimally on mobile devices. For the best experience, please use app on a desktop device. </p>
+          <h2 className="text-center">Miroslav</h2>
         </Modal>
         <div className="welcome-text" style={{paddingBottom:"5%"}}><h1>Welcome to League of Stats!</h1></div>
         <div class="search-box">
