1fe9c47841699be252931f42052120c0a8ac1c79
Miki <miroslav_simic@epam.com>
2023-05-16 19:15:35 +0200

    Border level icon fix

--- src/helpers/avatarlevels.js
+++ src/helpers/avatarlevels.js
@@ -1,5 +1,8 @@
 export const renderAvatarIcon = (summonerLevel) => {
-    const borderLevel = Math.min(500, Math.max(1, Math.floor(summonerLevel / 25) * 25));
+    let borderLevel = Math.min(500, Math.max(1, Math.floor(summonerLevel / 25) * 25));
+    if (summonerLevel < 30) borderLevel = 1;
+    if (summonerLevel >= 30 && summonerLevel < 50) borderLevel = 30;
+
     return `https://res.cloudinary.com/mistahpig/image/upload/v1621882189/league-stats/borders/lvl${borderLevel}.png`
   }
   
