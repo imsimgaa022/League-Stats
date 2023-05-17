4567b3ccb01820c35af3a739e5bf8674bbe84967
Miki <miroslav_simic@epam.com>
2023-05-17 17:07:45 +0200

    Netlify webhook test

--- netlify/functions/discord-webhook.js
+++ netlify/functions/discord-webhook.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 export async function handler(event, context) {
   const { commitId, commitMessage, branch } = JSON.parse(event.body);
 
-  // Customize the message content
   const message = `New deployment on branch ${branch}\nCommit ID: ${commitId}\nCommit Message: ${commitMessage}`;
-
+  console.log('test2')
   try {
-    // Send a POST request to the Discord webhook URL
-    await axios.post("https://discordapp.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: message });
+    await axios.post("https://discordapp.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: "Just a test deploy" });
 
     return {
       statusCode: 200,
