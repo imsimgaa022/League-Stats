090ee30fc8e9ae07eb92db131b5eb06063a6f0db
Miki <miroslav_simic@epam.com>
2023-05-18 14:10:03 +0200

    Message test

--- functions/discordWebhook.js
+++ functions/discordWebhook.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export async function handler(event, context) {
-    console.log(event,context);
   try {
     await axios.post("https://discord.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: 'Build started!!' });
 
