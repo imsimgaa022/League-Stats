550a1e25e8a9307704c82255aa798a8c2c1b129f
Miki <miroslav_simic@epam.com>
2023-05-18 14:27:04 +0200

    Deleted redirect file

--- functions/discordWebhook.js
+++ functions/discordWebhook.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function handler(event, context) {
   try {
-    await axios.post("https://discord.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: 'Build started!!' });
+    await axios.post("https://discord.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: 'Webhook test!' });
 
     return {
       statusCode: 200,
