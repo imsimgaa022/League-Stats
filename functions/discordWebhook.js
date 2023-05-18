fc54f0d923fff6a168574210376e5be496e0a68a
Miki <miroslav_simic@epam.com>
2023-05-18 13:47:12 +0200

    Does this even work

--- functions/discordWebhook.js
+++ functions/discordWebhook.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export async function handler(event, context) {
     console.log(event,context);
   try {
-    await axios.post("https://discord.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: 'This is just a test' });
+    await axios.post("https://discord.com/api/webhooks/1108403114144436234/De4boyqEmarsQ8qQJyXrt0jmpF--guZeANbdmbjNcFFRkl9MdDt_wJQpbAlh7qHi7nuG", { content: 'Build started!' });
 
     return {
       statusCode: 200,
